7523b117338f3084c2e1f45ee45b884a2d5e32c0
Syed Abdullah <syed.abdullah@bjitacademy.com>
2022-11-25 12:34:45 +0600

    updated view and class

--- app/src/main/java/com/syedabdullah/myapplication/MainActivity.kt
+++ app/src/main/java/com/syedabdullah/myapplication/MainActivity.kt
@@ -11,6 +11,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //dropdown work
+        val convertFrom= binding.spConvertFrom
+    }
+}
 
+abstract class Units{
+    abstract fun calculate(unit:String):Double
+}
+
+class Kilometer: Units() {
+    override fun calculate(unit: String):Double {
+        return 0.0
+    }
+}
+class Meter: Units() {
+    override fun calculate(unit: String): Double {
+        return 0.0
+    }
+}
+class Feet: Units() {
+    override fun calculate(unit: String): Double {
+        return 0.0
+    }
+}
+
+class Inche:Units(){
+    override fun calculate(unit: String): Double {
+        return 0.0
     }
 }
